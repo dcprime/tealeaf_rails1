f078de9f6a3ba0fc40988385f67607e5782e76c2
Darren Conley <dcprime@gmail.com>
2014-08-19 18:54:44 +0000

    Added authentication for views and controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,12 +8,4 @@ module ApplicationHelper
     dt.strftime("%b %d, %Y at %r - %Z")
   end
   
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  
-  def logged_in?
-    !!current_user
-  end
-  
 end
