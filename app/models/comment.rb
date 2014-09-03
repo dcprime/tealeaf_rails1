ff2bb264f56551ef8923c524322dfb64501be5d9
Darren Conley <dcprime@gmail.com>
2014-09-03 19:00:16 +0000

    extracted code to Voteable module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,27 +1,9 @@
 class Comment < ActiveRecord::Base
+  include Voteable
+  
   belongs_to :post
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
-  has_many :votes, as: :voteable
   
   validates :body, presence: true, length: {minimum: 3}
   
-  def vote_display
-    if self.total_votes == 1 || self.total_votes == -1
-      return " vote"
-    else
-      return " votes"
-    end
-  end
-  
-  def total_votes
-    up_votes - down_votes
-  end
-  
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-  
-  def down_votes
-    self.votes.where(vote: false).size
-  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,35 +1,16 @@
 class Post < ActiveRecord::Base
+  include Voteable
+  
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
   
   before_save :generate_slug!
   
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
-  
-  def vote_display
-    if self.total_votes == 1 || self.total_votes == -1
-      return " vote"
-    else
-      return " votes"
-    end
-  end
-  
-  def total_votes
-    up_votes - down_votes
-  end
-  
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-  
-  def down_votes
-    self.votes.where(vote: false).size
-  end
   
   def generate_slug!
     the_slug = to_slug(self.title)
